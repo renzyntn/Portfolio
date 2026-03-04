7b3f5f8089f462da007bfeb08bc9e363277120f4
Renzy Antonio <renzy.antonio0112@gmail.com>
2026-03-04 17:59:36 +0800

    chore: add and pass the following props from App.tsx to MainLayout to ChatPane component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,12 +3,30 @@ import type { MainLayoutProp } from "../types/proptypes";
 import ChatPane from "../components/ChatPane";
 import ChatButton from "../components/ChatButton";
 
-function MainLayout({ theme, isChat, setIsChat, toggleChat }: MainLayoutProp) {
+function MainLayout({
+  theme,
+  isChat,
+  setIsChat,
+  toggleChat,
+  chatHistory,
+  setChatHistory,
+  isLoading,
+  setIsLoading,
+}: MainLayoutProp) {
   return (
     <>
       <Outlet />
       <ChatButton theme={theme} isChat={isChat} toggleChat={toggleChat} />
-      {isChat && <ChatPane theme={theme} setIsChat={setIsChat} />}
+      {isChat && (
+        <ChatPane
+          theme={theme}
+          setIsChat={setIsChat}
+          chatHistory={chatHistory}
+          setChatHistory={setChatHistory}
+          isLoading={isLoading}
+          setIsLoading={setIsLoading}
+        />
+      )}
     </>
   );
 }
